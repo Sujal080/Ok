6f0e75cf29163d3e87faf9f674f851459decf42c
SUJAL CHAVDA <chavdasujal08@gmail.com>
2025-06-10 00:40:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from scraper import scrape_videos
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "✅ API is running. Visit /videos to get video data."}
+
 @app.get("/videos")
 def get_videos():
     return {"data": scrape_videos()}
